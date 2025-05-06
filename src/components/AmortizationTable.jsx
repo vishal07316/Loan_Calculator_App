c396ac3d0eb0f6570f8e480644cc88b76a70388f
vishal07316 <vishalkumarv2020@gmail.com>
2025-05-06 21:18:20 +0530

    initial setup completed

---
+++ src/components/AmortizationTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AmortizationTable = () => {
+  return (
+    <div>AmortizationTable</div>
+  )
+}
+
+export default AmortizationTable
---
+++ src/components/LoanForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoanForm = () => {
+  return (
+    <div>LoanForm</div>
+  )
+}
+
+export default LoanForm
