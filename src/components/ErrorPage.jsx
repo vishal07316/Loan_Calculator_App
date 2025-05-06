80a9dc162e35910c95fa792d5dc045e3a500cb8f
vishal07316 <vishalkumarv2020@gmail.com>
2025-05-06 21:22:42 +0530

    Error page finished

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box } from '@mui/material';
 
-const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
-}
+const ErrorPage = ({ error }) => (
+  <Box sx={{ p: 3, textAlign: 'center' }}>
+    <Typography variant="h5" color="error">
+      Something went wrong!
+    </Typography>
+    <Typography>{error || 'Please try again later.'}</Typography>
+  </Box>
+);
 
-export default ErrorPage
+export default ErrorPage;
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box } from '@mui/material';
 
-const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+const NotFound = () => (
+  <Box sx={{ p: 3, textAlign: 'center' }}>
+    <Typography variant="h5">404 - Page Not Found</Typography>
+    <Typography>The page you are looking for does not exist.</Typography>
+  </Box>
+);
 
-export default NotFound
+export default NotFound;
