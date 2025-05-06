8a9c5ceb25cb1f00c268b38bd7a2439511c7484a
vishal07316 <vishalkumarv2020@gmail.com>
2025-05-06 21:26:28 +0530

    context is added

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { createContext, useState } from 'react';
 
-const AppContext = () => {
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [emi, setEMI] = useState(null);
+  const [amortization, setAmortization] = useState([]);
+  const [themeMode, setThemeMode] = useState('light');
+
   return (
-    <div>AppContext</div>
-  )
-}
-
-export default AppContext
+    <AppContext.Provider value={{ emi, setEMI, amortization, setAmortization, themeMode, setThemeMode }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
